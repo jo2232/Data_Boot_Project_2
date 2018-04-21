cbc1d6c8ee4c43d1fff85468a1d3968bf845c6d7
Blaine Anspaugh <spaugh56@yahoo.com>
2018-04-21 12:50:12 -0500

    framework push
    framework push

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import tweepy1
+import google1
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route('/scrape')
+def scrape():
+    tweepy = mongo.db.tweepy
+    data = tweepy1.numbers()
+    tweepy.update(
+        {},
+        data,
+        upsert=True
+        )
+    google = mongo.db.google
+    data = google1.numbers()
+    google.update(
+        {},
+        data,
+        upsert=True
+        )
+    return redirect("http://127.0.0.1:5000/", code=302)
+
+@app.route('/')
+def index():
+    tweepy = mongo.db.tweepy.find_one()
+    google = mongo.db.google.find_one()
+    return render_template('index.html', tweepy=tweepy, google=google)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ google1.py
@@ -0,0 +1,13 @@
+import numpy as np
+from flask import jsonify
+
+def numbers():
+
+    mars_dict = {
+    'news_title' : 'THIS IS FROM GOOGLE MONGO DB HI MOM',
+    'news_summary' : 'hi', 
+    'featured_image_url' : 'hi',
+    'mars_weather' : 'hi',
+    'table_string' : 'hi',
+    'hemisphere_image_urls' : 'hi'}
+    return mars_dict
