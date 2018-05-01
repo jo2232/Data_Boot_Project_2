33960c5cd500033fe0b17dd952e0f32175d36acf
Flaw1331 <christian.ensz@gmail.com>
2018-05-01 00:02:48 -0500

    ScrapedV3 update - GPS, MONGO, cleaning

--- tweepy1.py
+++ tweepy1.py
@@ -3,11 +3,19 @@ import json
 import time
 import os
 import requests
-import twitter
+import twitter      # pip install python-twitter or twitter-python?
 import numpy as np
+import random
 
+# Setting up Vader
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
+
+# Setting up Mongo
+import pymongo
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.tweets_db
 
 # Setting Keys
 consumer_key = 'SZuyqX69qC9kH9juCBquOuA45'
@@ -26,43 +34,69 @@ api = twitter.Api(consumer_key=consumer_key,
 def tweetGrab(city, city_dict, start_time):
 
     # Call to API - each new call should return new tweets and count against rate-limit
-    tweets = api.GetSearch(geocode=[city_dict[f'{city}']['gps'][1],city_dict[f'{city}']['gps'][0], '20mi'],return_json=True)
+    tweets = api.GetSearch(geocode=[city_dict[f'{city}']['gps'][1],city_dict[f'{city}']['gps'][0], '20mi'],return_json=True,count=100)
 
     # Loop to parse tweet and append data needed
     for tweet in tweets['statuses']:
+        
+        # Temp dict do pass into function instead of whole data file
+        temp_dict = {
+            'tempText': [],
+            'tempBox': [],
+            'tempUser': [],
+            'tempCreated_at': [],
+            'tempFollowers_count': [],
+            'temp_id': [],
+            'tempComp': [],
+            'tempCords': [],
+            'tempImg': []         
+        }
 
         # If loop to ensure unique tweets
-        if tweet['text'] in city_dict[f'{city}']['data']['text']:
+        if tweet['id'] in city_dict[f'{city}']['data']['id']:
             continue
-        else:        
-            # Appending important data
-            city_dict[f'{city}']['data']['text'].append(tweet['text'])
-            city_dict[f'{city}']['data']['bounding_box'].append(solveBox(tweet['place']['bounding_box']['coordinates']))
-            city_dict[f'{city}']['data']['user'].append(tweet['user']['screen_name'])
-            city_dict[f'{city}']['data']['created_at'].append(tweet['created_at'])
-            city_dict[f'{city}']['data']['followers_count'].append(tweet['user']['followers_count'])
-            city_dict[f'{city}']['data']['comp_sent'].append(analyzer.polarity_scores(tweet['text'])['compound'])
-
+        else:
+            # Temp variables
+            temp_dict['tempText'].append(tweet['text'])
+            temp_dict['tempBox'].append(solveBox(tweet['place']['bounding_box']['coordinates']))
+            temp_dict['tempUser'].append(tweet['user']['screen_name'])
+            temp_dict['tempCreated_at'].append(tweet['created_at'])
+            temp_dict['tempFollowers_count'].append(tweet['user']['followers_count'])
+            temp_dict['temp_id'].append(tweet['id'])
+            temp_dict['tempComp'].append(analyzer.polarity_scores(tweet['text'])['compound'])
+           
             # Creating try loops for variable data that may or may not show up
             try:
-                city_dict[f'{city}']['data']['coords'].append(tweet['coordinates']['coordinates'])
+                temp_dict['tempCords'].append(tweet['coordinates']['coordinates'])
             except:
-                city_dict[f'{city}']['data']['coords'].append(tweet['coordinates'])
+                temp_dict['tempCords'].append(tweet['coordinates'])
 
             try:
-                city_dict[f'{city}']['data']['profile_image_url'].append(tweet['user']['profile_image_url'])
+                temp_dict['tempImg'].append(tweet['user']['profile_image_url'])
             except:
-                city_dict[f'{city}']['data']['profile_image_url'].append('None')
+                temp_dict['tempImg'].append('None')
+            
+            # Sending things over to Mongo for storage
+            updateMongo(temp_dict, city)
+            
+            # Appending important data from temp dict
+            city_dict[f'{city}']['data']['text'].append(temp_dict['tempText'][0])
+            city_dict[f'{city}']['data']['bounding_box'].append(temp_dict['tempBox'][0])
+            city_dict[f'{city}']['data']['user'].append(temp_dict['tempUser'][0])
+            city_dict[f'{city}']['data']['created_at'].append(temp_dict['tempCreated_at'][0])
+            city_dict[f'{city}']['data']['followers_count'].append(temp_dict['tempFollowers_count'][0])
+            city_dict[f'{city}']['data']['comp_sent'].append(temp_dict['tempComp'][0])
+            city_dict[f'{city}']['data']['id'].append(temp_dict['temp_id'][0])
+            city_dict[f'{city}']['data']['coords'].append(temp_dict['tempCords'][0])
+            city_dict[f'{city}']['data']['profile_image_url'].append(temp_dict['tempImg'][0])
+
 
             # Printing status of calls and current time
-            if (len(city_dict[f'{city}']['data']['text'])%5 == 0):
-                print(f'{city}: ' + str(len(city_dict[f'{city}']['data']['text'])) + ' --- %s seconds' % round(time.clock() - start_time,2))    
+            if (len(city_dict[f'{city}']['data']['id'])%5 == 0):
+                print(f'{city}: ' + str(len(city_dict[f'{city}']['data']['id'])) + ' --- %s seconds' % round(time.clock() - start_time,2))    
 
     # Ending function
     return()
-
-
-# In[5]:
 
 
 # Calculating a rough coord based on bounding box
@@ -74,8 +108,8 @@ def solveBox(bounding_box):
 
     # Small loop to append each coord to it's own list to sum
     for coord in bounding_box[0]:
-        coord_dict['lat'].append(coord[1] + (np.random.ranf() - np.random.ranf()))
-        coord_dict['lon'].append(coord[0] + (np.random.ranf() - np.random.ranf()))
+        coord_dict['lat'].append(coord[1] + random.uniform(-.005,.005))
+        coord_dict['lon'].append(coord[0] + random.uniform(-.005,.005))
 
     # Appending the sums to a return list
     coord_return.append(round(np.mean(coord_dict['lon']),6))
@@ -83,9 +117,6 @@ def solveBox(bounding_box):
 
     # Return list of coords
     return(coord_return)
-
-
-# In[6]:
 
 
 # Fills out the dictionary
@@ -102,9 +133,7 @@ def fillDict():
         'New York': {'gps':[-74.005973, 40.712775], 'data':{}},
         'Kansas City': {'gps':[-94.578567, 39.099727], 'data':{}},
         'Seattle': {'gps':[-122.332071, 47.606210], 'data':{}},
-        'Las Vegas': {'gps':[-115.139830, 36.169941], 'data':{}},
-
-
+        'Las Vegas': {'gps':[-115.139830, 36.169941], 'data':{}}
         }
 
     # Adding template to dictionary
@@ -119,12 +148,10 @@ def fillDict():
         city_dict[f'{city}']['data']['created_at'] = []
         city_dict[f'{city}']['data']['comp_sent'] = []
         city_dict[f'{city}']['data']['followers_count'] = []
+        city_dict[f'{city}']['data']['id'] = []
 
     # Returning the dictionary
     return(city_dict)
-
-
-# In[7]:
 
 
 # Function to compress limit check
@@ -159,18 +186,12 @@ def limit_check(check, city_dict, tweet_goal):
     return(limit)
 
 
-# In[8]:
-
-
 # Save output to a txt/json doc for easy read later
 def saveOutput(data, tweet_goal, start_time, limit_type):
     with open('data.txt', 'w') as outfile:
         json.dump(data, outfile, sort_keys = True, indent = 2)
     with open(f'{limit_type} - {tweet_goal} Tweets - ' + str(round(time.clock()-start_time,2)) + f' Runtime - {start_time}.txt', 'w') as outfile:
         json.dump(data, outfile, sort_keys = True, indent = 2)
-
-
-# In[9]:
 
 
 # Print Final Output
@@ -185,17 +206,28 @@ def printOutput(city_dict, start_time):
     
     # Final Format Print with runtime
     print('-------------------------------------------')
-    print(' --- %s Runtime' % round(time.clock() - start_time,2))
+    print('--- %s Runtime' % round(time.clock() - start_time,2))
 
-
-# In[10]:
-
+# Update to MongoDB
+def updateMongo(temp_dict, city):
+    db.city.insert_one({
+        'bounding_box': temp_dict['tempBox'][0], 
+        'cords': temp_dict['tempCords'][0], 
+        'comp_sent': temp_dict['tempComp'][0], 
+        'created_at': temp_dict['tempCreated_at'][0],
+        'followers_count': temp_dict['tempFollowers_count'][0],
+        'profile_image_url': temp_dict['tempImg'][0],
+        'text': temp_dict['tempText'][0],
+        'user': temp_dict['tempUser'][0],
+        'tweet_id': temp_dict['temp_id'][0],
+        'city': city
+    })
 
 # Main Execution
 def twitterize():
     
     # Setting up statics
-    tweet_goal = 20
+    tweet_goal = 100
     limit_type = 'first_to_goal' # 'first_to_goal' or 'all_to_goal'
     start_time = time.clock()
     
@@ -217,8 +249,3 @@ def twitterize():
     printOutput(city_dict, start_time)
 
     return(city_dict)
-
-
-
-
-
